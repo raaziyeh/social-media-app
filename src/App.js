7e6f79997f60fccc63aabec4b2690cbd4e0fc9d4
raziyeh <116181564+raaziyeh@users.noreply.github.com>
2022-11-25 19:27:48 +0330

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Login from "./pages/login/Login"
+
 function App() {
   return (
     <div className="App">
-        app
+        <Login />
     </div>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,30 @@
 import "./login.scss"
 
 const Login = () => {
-  return (
-    <div className="login">Login</div>
-  )
+	return (
+		<div className="login">
+			<div className="card">
+				<div className="left">
+					<h2>Hello World.</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation.
+					</p>
+					<span>Don't you have an account?</span>
+					<button>Register</button>
+				</div>
+				<div className="right">
+					<h1>Login</h1>
+					<form>
+						<input type="text" placeholder="Username" />
+						<input type="password" placeholder="Password" />
+						<button>Login</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Login
