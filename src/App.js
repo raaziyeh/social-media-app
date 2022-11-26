49b445d0748f054d900b3b934d5936e3b9fbbaa0
raziyeh <116181564+raaziyeh@users.noreply.github.com>
2022-11-26 19:15:14 +0330

    Add register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 
 function App() {
-  return (
-    <div className="App">
-        <Login />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Register />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,33 @@
 import "./register.scss"
 
 const Register = () => {
-  return (
-    <div className="register">Register</div>
-  )
+	return (
+		<div className="register">
+			<div className="card">
+				<div className="left">
+					<h1>Register</h1>
+					<form>
+						<input type="text" placeholder="Name" />
+						<input type="text" placeholder="Surname" />
+						<input type="email" placeholder="Email" />
+						<input type="text" placeholder="Username" />
+						<input type="password" placeholder="Password" />
+						<button>Register</button>
+					</form>
+				</div>
+				<div className="right">
+					<h2>Hello World.</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation.
+					</p>
+					<span>Do you have an account?</span>
+					<button>Login</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Register
