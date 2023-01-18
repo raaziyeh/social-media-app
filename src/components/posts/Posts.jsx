e05f1fdcf05d35d6645a56d87dd468acb68ad0c7
“raaziyehgit config --global user.email “rsoradimoghaddam@gmail.com <rsoradimoghaddam@gmail.com>
2023-01-18 12:25:40 +0330

    Fix the little horizontal scroll issue

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -59,6 +59,34 @@ const posts = [
 			},
 		],
 	},
+	{
+		id: 3,
+		name: "Joe Doe",
+		userId: 1,
+		profilePic:
+			"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+		img: "https://images.pexels.com/photos/5778899/pexels-photo-5778899.jpeg?auto=compress&cs=tinysrgb&w=600",
+		comments: [
+			{
+				id: 1,
+				postId: 1,
+				name: "John Doe",
+				text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation.`,
+				img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+			},
+			{
+				id: 2,
+				postId: 1,
+				name: "John Doe",
+				text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et d`,
+				img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+			},
+		],
+	},
 ]
 
 const Posts = () => {
